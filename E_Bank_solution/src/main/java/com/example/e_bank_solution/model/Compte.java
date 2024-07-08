f350729f4422084428c68b706e1d26d659510239
Abourkia_Chihab <abourkiachaab@gmail.com>
2024-07-08 11:11:19 +0100

    creation de compteSerivice

--- E_Bank_solution/src/main/java/com/example/e_bank_solution/model/Compte.java
+++ E_Bank_solution/src/main/java/com/example/e_bank_solution/model/Compte.java
@@ -1,21 +1,26 @@
 package com.example.e_bank_solution.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Date;
+@NoArgsConstructor
+@AllArgsConstructor
     @Getter
     @Setter
     @Entity
     public class Compte {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+        private int id;
         private String type;
-        private Double solde;
-        private Date dateCreation;
-        private String raisonFermeture;
+        private float solde;
+        private LocalDate dateCreation;
+
 
 
     }
--- E_Bank_solution/src/main/java/com/example/e_bank_solution/service/CompteService.java
+++ E_Bank_solution/src/main/java/com/example/e_bank_solution/service/CompteService.java
@@ -1,5 +1,23 @@
 package com.example.e_bank_solution.service;
 
+import com.example.e_bank_solution.model.Compte;
+import com.example.e_bank_solution.repository.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
 public class CompteService {
-    
+    @Autowired
+    private CompteRepository compteRepository;
+    public compte creatcompt(int id, String type, float solde) {
+        Compte compt = new Compte();
+        compt.setType(type);
+        compt.setSolde(solde);
+        compt.setDateCreation(LocalDate.now());
+        compt.setUtilisateur(userRepository.getReferenceById(id));
+        return CompteRepository.save(compt);
+
+    }
 }
